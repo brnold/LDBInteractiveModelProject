b689ef58abbeec80bb448bffcede226534960d1f
Benjamin Nold <brnold@oakland.edu>
2016-06-07 09:51:26 -0400

    Updated Repo, readme

--- pyGamesTest.py
+++
@@ -1,8 +0,0 @@
-import pygame
-pygame.init()
-song = pygame.mixer.Sound('thesong.ogg')
-clock = pygame.time.Clock()
-song.play()
-while True:
-    clock.tick(60)
-pygame.quit()
--- testPyglet.py
+++
@@ -1,6 +0,0 @@
-import pyglet
-
-music = pyglet.resource.media('bubbling_water_1.mp3')
-music.play()
-
-pyglet.app.run()
